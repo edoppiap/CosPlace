2036d78451588b53ef30e48e7539fe306a63fc7b
Emanuele Pietropaolo <edoppiap@gmail.com>
2023-07-02 04:30:25 +0200

    Try VICRegLoss 4

--- train.py
+++ train.py
@@ -178,7 +178,7 @@ for epoch_num in range(start_epoch_num, args.epochs_num):
         
         if args.augmentation_device == "cuda":
             images = gpu_augmentation(images)
-            if args.loss == 'TripletMarginLoss' or args.loss == 'VICRegLoss':
+            if args.loss == 'TripletMarginLoss':
                 augmented = gpu_augmentation_2(images)    
                 
         
@@ -188,7 +188,7 @@ for epoch_num in range(start_epoch_num, args.epochs_num):
         if not args.use_amp16:
             descriptors = model(images)
             output = classifiers[current_group_num](descriptors, targets)
-            if args.loss == 'TripletMarginLoss' or args.loss == 'VICRegLoss':
+            if args.loss == 'TripletMarginLoss':
                 augmented_descriptors = model(augmented)
                 augmented_output = classifiers[current_group_num](augmented_descriptors, targets)
                 loss = criterion(descriptors, augmented_descriptors, targets)
